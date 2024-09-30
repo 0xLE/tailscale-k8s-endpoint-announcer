5c9f0903b1ab6c35170a486d3774d22a2aad240d
0xLE <78134270+0xLE@users.noreply.github.com>
2024-09-30 21:02:35 +0200

    fix: fix additional tailscale routes feature

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -x
 
 routes=$(kubectl get nodes "$KUBE_NODE_NAME" -o jsonpath='{.spec.podCIDR}')
 
-if [ -z "$TS_ADDITIONAL_ROUTES" ]; then
+if [ -n "$TS_ADDITIONAL_ROUTES" ]; then
   routes="$routes,$TS_ADDITIONAL_ROUTES"
 fi
 
