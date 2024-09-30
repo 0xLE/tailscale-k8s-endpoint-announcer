a78d76d97c87dd153408d4de01bc478cae5e2330
0xLE <78134270+0xLE@users.noreply.github.com>
2024-09-30 20:52:58 +0200

    feat: allow for additional tailscale subnet routes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,10 @@
 set -e
 set -x
 
-cidr=$(kubectl get nodes "$KUBE_NODE_NAME" -o jsonpath='{.spec.podCIDR}')
-tailscale set --advertise-routes="$cidr"
+routes=$(kubectl get nodes "$KUBE_NODE_NAME" -o jsonpath='{.spec.podCIDR}')
+
+if [ -z "$TS_ADDITIONAL_ROUTES" ]; then
+  routes="$routes,$TS_ADDITIONAL_ROUTES"
+fi
+
+tailscale set --advertise-routes="$routes"
